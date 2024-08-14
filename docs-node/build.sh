b11a719ce1f1a6aef578ae59d0b51a0fdbd16c25
Brandon Ly <brandon.ly@mongodb.com>
2024-08-14 11:41:49 -0500

    Update build.sh

--- docs-node/build.sh
+++ docs-node/build.sh
@@ -3,5 +3,5 @@ if [ -f build-site.sh ]; then
   rm build-site.sh
 fi 
 
-curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-site.sh -o build-site.sh 
+curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc-prefix/scripts/build-site.sh -o build-site.sh 
 sh build-site.sh
